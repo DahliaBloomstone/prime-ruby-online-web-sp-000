c73509a4363db33fc51e3782dbd7e5a0b4cd828b
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-20 23:06:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,7 @@ def prime?(number)
   start = 2
   if number > 1
     range = (start..number-1).to_a
-    range.none? do |num_test| 
+    range.none? do |num_test|
       number % num_test == 0
     end
   else
